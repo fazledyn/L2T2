82593ea2b0176a0c0b3e483f1820ca43e989c30e
fazledyn <1705066@ugrad.cse.buet.ac.bd>
2020-03-06 23:28:40 +0600

    Offline 2 completed

--- CSE 208/Offline2.cpp
+++ CSE 208/Offline2.cpp
@@ -3,9 +3,11 @@
 #include <queue>
 #include <limits>
 #include <fstream>
+#include <cstdio>
+
 using namespace std;
 
-#define INF 99999999
+#define INF 999999999
 
 
 class Graph {
@@ -13,6 +15,7 @@ class Graph {
        vector<pair<int, int>> *adjList;
        int *dist;
        bool *visited;
+       int *parent;
        int nVertices;
 
        public:
@@ -20,6 +23,7 @@ class Graph {
               nVertices = n;
               adjList = new vector<pair<int, int>>[nVertices];
               dist = new int[nVertices];
+              parent = new int[nVertices];
               visited = new bool[nVertices];
        }
 
@@ -41,46 +45,51 @@ class Graph {
               adjList[u].push_back(make_pair(v, w));
        }
 
-       void DijkstraAlgorithm(int src, int des){
-              cout << "Dijkstra's Algorithm :" << endl;
+       void printTraceback(FILE *fp, int src, int des){
+              if (des > 0) {
+                     printTraceback(fp, src, parent[des]);
+                     fprintf(fp, " -> %d", des);
+              } else if (des == src) {
+                     fprintf(fp, "%d", des);
+              }
+       }
+
+       void DijkstraAlgorithm(FILE *fp, int src, int des){
               int u;
               priority_queue<int, vector<int>, greater<int>> queue;
 
               for (int i=0; i<nVertices; i++){
                      dist[i] = INF;
                      visited[i] = false;
-                     queue.push(i);
+                     parent[i] = -1;
               }
-
+              
+              queue.push(src);
               dist[src] = 0;
+
               while (!queue.empty()){
                      u = queue.top();
                      queue.pop();
-
-                     cout << u << endl;
-                     visited[u] = true;
                      
+                     visited[u] = true;  
                      int v, w;
                      for (int i=0; i < adjList[u].size(); i++){
                             v = adjList[u].at(i).first;
-                            w = adjList[u].at(i).second;
+                            w = abs(adjList[u].at(i).second);
 
                             if (dist[v] > dist[u] + w){
-                                   cout << "went in" << endl;
+                                   parent[v] = u;
                                    dist[v] = dist[u] + w;
+                                   queue.push(v);
                             }
                      }
               }
-              if (dist[des] != INF){
-                     cout << "Distance of " << src << " -> " << des << ": " << dist[des] << endl;
-              } else {
-                     cout << src << " -> " << des << endl;
-                     cout << "There's no shortest path" << endl;
-              }
+              fprintf(fp, "Dijkstra Algorithm:\n");
+              fprintf(fp, "%d\n", dist[des]);
+              printTraceback(fp, src, des);
        }
 
-       void BellmanFord(int src, int des){
-              cout << "Bellman Ford Algorithm :" << endl;
+       void BellmanFord(FILE *fp, int src, int des){
               for (int i=0; i<nVertices; i++){
                      dist[i] = INF;
               }
@@ -95,6 +104,7 @@ class Graph {
 
                                    if (dist[v] > dist[u] + w){
                                           dist[v] = dist[u] + w;
+                                          parent[v] = u;
                                    }
                             }
                      }
@@ -107,56 +117,73 @@ class Graph {
                             w = adjList[j].at(k).second;
 
                             if (dist[v] > dist[u] + w){
-                                   cout << "Negative Edge Detected !" << endl;
+                                   fprintf(fp, "Negative edge cycle detected !");
                             }
                      }
               }
 
-              if (dist[des] != INF){
-                     cout << "Distance of " << src << " -> " << des << ": " << dist[des] << endl;
-              } else {
-                     cout << src << " -> " << des << endl;
-                     cout << "There's no shortest path" << endl;
-              }
+              fprintf(fp, "Bellman Ford Algorithm:\n");
+              fprintf(fp, "%d\n", dist[des]);
+              printTraceback(fp, src, des);
        }
 };
 
 
 int main(){
 
-       Graph g(10);
+       FILE *file;
+       file = fopen("input.txt", "r");
 
-       g.addEdge(0, 7, 60);
-       g.addEdge(7, 1, -150);
-       g.addEdge(4, 8, -70);
-       g.addEdge(6, 4, 80);
-       g.addEdge(5, 1, 4000);
-       g.addEdge(8, 0, 100000);
-       g.addEdge(2, 3, -200);
-       g.addEdge(8, 2, 1000);
+       int vertices, edges;
+       fscanf(file, "%d", &vertices);
+       fscanf(file, "%d", &edges);
 
-       g.addEdge(0, 3, 300);
-       g.addEdge(3, 8, 50000);
-       g.addEdge(3, 7, -200);
-       g.addEdge(2, 5, 120);
-       g.addEdge(6, 3, 1500);
-       g.addEdge(4, 0, 90);
+       Graph g(vertices);
+       int first, second, weight;
 
-       g.addEdge(5, 7, -50);
-       g.addEdge(1, 6, 100);
-       g.addEdge(4, 1, -90);
-
-       g.printGraph();
-  //     fstream inputFile;
-//       inputFile.open("input.txt", ios::in);
-
-       g.DijkstraAlgorithm(0, 5);
-       cout << endl;
-       g.printDist();
+       for (int i=0; i < edges; i++){
+              fscanf(file, "%d", &first);
+              fscanf(file, "%d", &second);
+              fscanf(file, "%d", &weight);
+              g.addEdge(first, second, weight);
+       }
        
-       cout << endl;
-       g.BellmanFord(0, 5);
+       int src, des;       
+       fscanf(file, "%d", &src);
+       fscanf(file, "%d", &des);
+       fclose(file);
+
+       // Graph g(10);
+
+       // g.addEdge(0, 7, 60);
+       // g.addEdge(7, 1, -150);
+       // g.addEdge(4, 8, -70);
+       // g.addEdge(6, 4, 80);
+       // g.addEdge(5, 1, 4000);
+
+       // g.addEdge(8, 0, 100000);
+       // g.addEdge(2, 3, -200);
+       // g.addEdge(8, 2, 1000);
+       // g.addEdge(0, 3, 300);
+       // g.addEdge(3, 8, 50000);
+       // g.addEdge(3, 7, -200);
        
+       // g.addEdge(2, 5, 120);
+       // g.addEdge(6, 3, 1500);
+       // g.addEdge(4, 0, 90);
+       // g.addEdge(5, 7, -50);
+       // g.addEdge(1, 6, 100);
+       // g.addEdge(4, 1, -90);
+
+       file = fopen("output.txt", "w");
+
+       g.BellmanFord(file, src, des);
+       fprintf(file, "\n \n");
+       g.DijkstraAlgorithm(file, src, des);
+
+       fclose(file);
+
+       cout << "The output file has been generated !" << endl;
        cin.ignore();
 
        return 0;
