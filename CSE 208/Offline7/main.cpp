f696aabf32afa53ef98399d97c7299333a16ba04
fazledyn <1705066@ugrad.cse.buet.ac.bd>
2020-11-12 01:02:28 +0600

    Works fine

--- CSE 208/Offline7/main.cpp
+++ CSE 208/Offline7/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+#include <assert.h>
 using namespace std;
 
 #define INFINITY INT_MAX
@@ -11,7 +12,7 @@ public:
     int key, degree;
     Node *parent, *child, *sibling;
 
-    Node (int _key)
+    Node(int _key)
     {
         key = _key;
         degree = 0;
@@ -19,24 +20,60 @@ public:
         child = nullptr;
         sibling = nullptr;
     }
+
+    Node() {
+        degree = 0;
+        parent = nullptr;
+        child = nullptr;
+        sibling = nullptr;
+    }
+
+    void print() {
+        cout << endl;
+        cout << "Printing Node: " << endl;
+        cout << "Key: " << key << endl;
+        cout << "Degree: " << degree << endl;
+        if (parent != nullptr)
+            cout << "Parent: " << parent->key << "(" << parent << ")" << endl;
+        if (child != nullptr)
+            cout << "Child: " << child->key << "(" << child << ")" << endl;
+        if (sibling != nullptr)
+            cout << "Sibling: " << sibling->key << "(" << sibling << ")" << endl;
+        cout << endl;
+    }
+
 };
 
-class BinomialHeap {
+class BinomialHeap
+{
     Node *head;
 
 public:
-    Node *findMinimum() {
+
+    BinomialHeap() {
+        head = nullptr;
+    }
+
+    Node* getHead() {
+        return this->head;
+    }
+
+    Node *findMinimum()
+    {
         Node *y = nullptr;
         Node *x = head;
         int minimum = INFINITY;
 
-        while (x != nullptr) {
-            if (x->key < minimum) {
+        while (x != nullptr)
+        {
+            if (x->key < minimum)
+            {
                 minimum = x->key;
                 y = x;
             }
             x = x->sibling;
         }
+        return y;
     }
 
     void BinomialLink(Node *y, Node *z)
@@ -47,125 +84,174 @@ public:
         z->degree = z->degree + 1;
     }
 
-    void BinomialInsert(Node* x) {
+    void BinomialInsert(Node *x)
+    {
         BinomialHeap newHeap;
         newHeap.head = x;
-        this->BinomialHeapUnion(newHeap);
+        this->BinomialUnion(newHeap);
     }
 
-    Node* BinomialHeapMerge(BinomialHeap heap) {
-        Node* thisHead = this->head;
-        Node* thatHead = heap.head;
-        Node* newHead = nullptr;
-        Node* tempHead = nullptr;
+    void BinomialDecreaseKey(Node *node, int newKey)
+    {
+        if (newKey > node->key)
+        {
+            cout << "The new key is greater than the previous one!" << endl;
+        }
+        else
+        {
+            node->key = newKey;
+            Node *temp = node;
+            Node *parent = temp->parent;
 
-        if (thisHead->degree <= thatHead->degree) {
+            while ((parent != nullptr) && (temp->key < parent->key))
+            {
+                swap(temp->key, parent->key);
+                // if temp and parent has satelite fields and exchange them too ??????????????
+                // wtf does this mean
+                temp = parent;
+                parent = temp->parent;
+            }
+        }
+    }
+
+    void BinomialDelete(Node *x)
+    {
+        BinomialDecreaseKey(x, -INFINITY);
+        BinomialExtractMin();
+    }
+
+    void BinomialHeapMerge(BinomialHeap heap)
+    {
+        Node *thisHead = this->head;
+        Node *thatHead = heap.head;
+        Node *newHead = nullptr;
+        Node *storeHead = nullptr;
+
+        if (thisHead == nullptr) {
+            head = heap.head;
+            return;
+        }
+
+        if (thisHead->degree <= thatHead->degree)
+        {
             newHead = thisHead;
             thisHead = thisHead->sibling;
         }
-        else {
+        else
+        {
             newHead = thatHead;
             thatHead = thatHead->sibling;
         }
-        tempHead = newHead;
+        storeHead = newHead;
 
-        while (thisHead != nullptr && thatHead != nullptr) {    
-            if (thisHead->degree <= thatHead->degree) {
+        while (thisHead != nullptr && thatHead != nullptr)
+        {
+            if (thisHead->degree <= thatHead->degree)
+            {
                 newHead->sibling = thisHead;
                 thisHead = thisHead->sibling;
             }
-            else {
+            else
+            {
                 newHead->sibling = thatHead;
                 thatHead = thatHead->sibling;
             }
             newHead = newHead->sibling;
         }
 
-        if (thisHead != nullptr) {
-            while (thisHead != nullptr) {
+        if (thisHead != nullptr)
+        {
+            while (thisHead != nullptr)
+            {
                 newHead->sibling = thisHead;
                 thisHead = thisHead->sibling;
                 newHead = newHead->sibling;
             }
         }
 
-        if (thatHead != nullptr) {
-            while (thatHead != nullptr) {
+        if (thatHead != nullptr)
+        {
+            while (thatHead != nullptr)
+            {
                 newHead->sibling = thatHead;
                 thatHead = thatHead->sibling;
                 newHead = newHead->sibling;
             }
         }
 
-        cout << "\nreturning newHead ..." << endl;
-        cout << "newHead: " << newHead << endl;
-        return newHead;
+        this->head = storeHead;
     }
 
 
-    void BinomialHeapUnion(BinomialHeap heap) {
+    void BinomialUnion(BinomialHeap heap) {
 
-        BinomialHeap newHeap;
-        newHeap.head = this->BinomialHeapMerge(heap);
+        BinomialHeapMerge(heap);
 
-        if (newHeap.head == nullptr) {
-            this->head = newHeap.head;
+/*
+        if (this->head == nullptr) {
+            return;
         }
+*/
 
         Node* prev = nullptr;
-        Node* x = newHeap.head;
-        Node* next = x->sibling;
+        Node* curr = this->head;
+        Node* next = curr->sibling;
 
         while (next != nullptr) {
-            if ( (x->degree != next->degree) || ((next->sibling != nullptr) && (next->sibling->degree == x->degree)) ) {
-                prev = x;
-                x = next;
+
+            if ( (curr->degree != next->degree) || ((next->sibling != nullptr) && (next->sibling->degree == curr->degree)) ) {
+                prev = curr;
+                curr = next;
             }
             else {
-                if (x->key <= next->key) {
-                    x->sibling = next->sibling;
-                    this->BinomialLink(next, x);
+
+                if (curr->key <= next->key) {
+                    curr->sibling = next->sibling;
+                    BinomialLink(next, curr);
                 }
                 else {
                     if (prev == nullptr) {
-                        this->head = next;
+                        head = next;
                     }
                     else {
                         prev->sibling = next;
                     }
-                    this->BinomialLink(x, next);
-                    x = next;
+                    BinomialLink(curr, next);
+                    curr = next;
                 }
             }
-            next = x->sibling;
+            next = curr->sibling;
         }
     }
 
-    void ExtractMin() {
-
+    void BinomialExtractMin()
+    {
         Node *minNode = findMinimum();
         Node *child = minNode->child;
 
         // Parent Null kore
-        while (child != nullptr) {
+        while (child != nullptr)
+        {
             child->parent = nullptr;
             child = child->sibling;
         }
 
         child = minNode->child;
-        stack<Node*> allSibling;
+        stack<Node *> allSibling;
 
-        while (child != nullptr) {
+        while (child != nullptr)
+        {
             allSibling.push(child);
             child = child->sibling;
         }
 
-        Node* currHead = allSibling.top();
+        Node *currHead = allSibling.top();
         child = currHead;
         allSibling.pop();
 
         // resort them to the order
-        while ( !allSibling.empty() ) {
+        while (!allSibling.empty())
+        {
             child->sibling = allSibling.top();
             allSibling.pop();
             child = child->sibling;
@@ -176,10 +262,30 @@ public:
         BinomialHeap newHeap;
         newHeap.head = currHead;
 
-        this->BinomialHeapUnion(newHeap);
+        this->BinomialUnion(newHeap);
     }
 
-    
+
 
 
 };
+
+
+
+int main() {
+
+    BinomialHeap bh;
+    Node* node;
+
+    node = new Node(2);
+    bh.BinomialInsert(node);
+
+    node = new Node(5);
+    bh.BinomialInsert(node);
+
+    node = new Node(4);
+    bh.BinomialInsert(node);
+
+    bh.getHead()->print();
+    bh.getHead()->sibling->print();
+}
